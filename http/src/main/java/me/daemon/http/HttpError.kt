bf50014ea44bd4db82c5c611d369be384d6a2f7c
daemon369 <daemon336699@gmail.com>
2019-07-09 11:52:25 +0800

    refactor HttpError

--- http/src/main/java/me/daemon/http/HttpError.kt
+++ http/src/main/java/me/daemon/http/HttpError.kt
@@ -4,19 +4,37 @@ package me.daemon.http
  * @author daemon
  * @since 2019-07-06 23:06
  */
-class HttpError(message: String?, cause: Throwable?) : Throwable(message, cause) {
+class HttpError(builder: Builder) : Throwable(builder.message, builder.cause) {
 
-    var httpCode = 200
-    var previous: HttpError? = null
+    val httpCode = builder.httpCode
+    val previous: HttpError? = builder.previous
 
-    constructor(message: String?) : this(message, null)
+    class Builder() {
+        internal var message: String? = null
+        internal var cause: Throwable? = null
+        internal var httpCode: Int = 200
+        internal var previous: HttpError? = null
 
-    constructor(cause: Throwable?) : this(cause?.toString(), cause)
+        constructor(httpError: HttpError) : this() {
+            this.message = httpError.message
+            this.cause = httpError.cause
+            this.httpCode = httpError.httpCode
+            this.previous = httpError.previous
+        }
 
-    constructor() : this(null, null)
+        fun message(message: String?) = apply { this.message = message }
+        fun cause(cause: Throwable?) = apply { this.cause = cause }
+        fun httpCode(httpCode: Int) = apply { this.httpCode = httpCode }
+        fun previous(previous: HttpError?) = apply { this.previous = previous }
 
-    fun httpCode(httpCode: Int) = apply { this.httpCode = httpCode }
+        fun build() = HttpError(this)
+    }
 
-    fun previous(previous: HttpError?) = apply { this.previous = previous }
-
+    companion object {
+        fun builder() = Builder()
+        fun builder(message: String) = builder().message(message)
+        fun builder(cause: Throwable) = builder().cause(cause)
+        fun builder(message: String, cause: Throwable) = builder().message(message).cause(cause)
+        fun builder(httpError: HttpError) = Builder(httpError)
+    }
 }
